2aa9c6ef9a6aa10a63cc9cfeac3485c90d888db9
Quasar Jarosz <quasar@ja.rosz.org>
2015-09-30 22:38:09 -0500

    forgot to commit uploader changes

--- uploader.py
+++ uploader.py
@@ -1,30 +1,39 @@
 import paramiko
 import progressbar
 
+def make_progress_callback():
+    bar = None
 
-def progress(current, total):
-    if progress.bar is None:
-        widgets = [progressbar.Percentage(),
-                   progressbar.Bar(),
-                   progressbar.ETA(),
-                   progressbar.FileTransferSpeed()]
-        progress.bar = progressbar.ProgressBar(widgets=widgets, maxval=total)
-        progress.bar.start()
-    if current == total:
-        progress.bar.finish()
-    else:
-        progress.bar.update(current)
-progress.bar = None
+    def progress(current, total):
+        nonlocal bar
+        if bar is None:
+            widgets = [progressbar.Percentage(), ' ',
+                       progressbar.Bar(), ' ',
+                       progressbar.ETA(), ' ',
+                       progressbar.FileTransferSpeed()]
+            bar = progressbar.ProgressBar(widgets=widgets, maxval=total)
+            bar.start()
+        if current == total:
+            bar.finish()
+        else:
+            bar.update(current)
+
+    return progress
 
 
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+def upload(source_filename, dest_filename):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-client.connect("quasarj.com", username="truckload", password="4hg0sn33k")
-sftp = client.open_sftp()
+    client.connect("quasarj.com", username="truckload", password="4hg0sn33k")
+    sftp = client.open_sftp()
 
-print("Connected, beginning upload...")
-sftp.put('nerds.webm', 'tubes/nerds.webm', progress)
+    # print("Connected, beginning upload...")
+    sftp.put(source_filename, 'tubes/' + dest_filename, make_progress_callback())
 
-print("Complete, exiting")
-client.close()
+    # print("Complete, exiting")
+    client.close()
+
+
+if __name__ == '__main__':
+    upload('nerds.webm', 'nerds.webm')
