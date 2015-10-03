ee2d7f53bb75c3fa965c02f1f47954a5483ebc9f
Quasar Jarosz <quasar@ja.rosz.org>
2015-10-02 22:06:37 -0500

    Finally actually working

--- maker.py
+++ maker.py
@@ -1,35 +1,59 @@
 import win32gui
+import win32api
 import os
 import subprocess
+import progressbar
+import signal
 
 import hotpy
 import uploader
 import namer
 
 
+def feed_file_to_handle_with_progress(filename, handle):
+    chunk_size = 1024
+    position = 0
+    total = os.path.getsize(filename)
+
+    widgets = [progressbar.Percentage(), ' ',
+               progressbar.Bar(), ' ',
+               progressbar.ETA(), ' ',
+               progressbar.FileTransferSpeed()]
+    bar = progressbar.ProgressBar(widgets=widgets, maxval=total)
+    bar.start()
+
+    with open(filename, 'rb') as f:
+        while True:
+            chunk = f.read(chunk_size)
+            if not chunk:
+                handle.close()
+                bar.finish()
+                break
+            handle.write(chunk)
+            position += len(chunk)
+            bar.update(position)
+
+
+
 def cleanup():
     try:
-        os.unlink('test-out.avi')
+        os.unlink('test-out2.avi')
         os.unlink('test-out.webm')
     except FileNotFoundError:
         pass
 
 def encode_video():
     p = subprocess.Popen('ffmpeg\\ffmpeg.exe '
-            '-i test-out.avi '
+            '-i - '
             '-c:v libvpx -b:v 10000k '
             'test-out.webm',
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print("Encoding has started. This will take a while...")
+            bufsize=64,
+        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
+        stdin=subprocess.PIPE)
+    print("Encoding...")
+    feed_file_to_handle_with_progress('test-out2.avi', p.stdin)
     p.wait()
 
-
-def get_rect(hwnd):
-    x, y, x2, y2 = win32gui.GetWindowRect(hwnd)
-    w = x2 - x
-    h = y2 - y
-
-    return x, y, w, h
 
 def get_title(hwnd):
     return win32gui.GetWindowText(hwnd)
@@ -45,11 +69,13 @@ def start_capture():
     title = get_title(window)
 
     cleanup()
+    print("cleanup finished, calling subprocess")
     PID = subprocess.Popen('ffmpeg\\ffmpeg.exe -f gdigrab -i title="{}" '
                 '-framerate 15 -vf "scale=\'iw/2\':-1" '
                 '-c:v rawvideo -pix_fmt yuv420p '
-                'test-out.avi'.format(title),
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                'test-out2.avi'.format(title),
+        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
+        stdin=subprocess.PIPE)
     # out, err = PID.communicate()
 
     print("Capturing has begun. Press F9 again to stop!")
@@ -57,8 +83,13 @@ def start_capture():
 def stop_capture():
     global PID
 
-    PID.terminate()
+    #PID.terminate()
+    #win32api.TerminateProcess(int(PID._handle), -1)
+    #os.system('TASKKILL /PID {}'.format(PID.pid))
+    PID.stdin.write(b'q')
+    PID.stdin.flush()
     PID.wait()
+    PID = None
 
     name = '{}.webm'.format(namer.get_name())
     url = 'http://i.notabigtruck.com/i/tubes/{}'.format(name)
@@ -71,11 +102,11 @@ def stop_capture():
 
     print('\n\n{}\n\n'.format(url))
     os.system('start {}'.format(url))
-    
+
 
 def handle_f9():
     global PID
-    
+
     if PID is None:
         start_capture()
     else:
@@ -83,12 +114,37 @@ def handle_f9():
 
 
 def main():
-    hotpy.register(handle_f9, 'F9')
+    hotpy.register(handle_f9, 'F9', ['Alt'])
     hotpy.register(lambda: False, 'F9', ['Ctrl'])  # exit
 
-    print("Press F9 to start recording!")
+    print("A Series of Tubes, v0.1")
+    print("A simple webm maker, by Quasar\n")
+
+    print("Press Alt+F9 to start recording!")
+    print("Press Ctrl+F9 to exit.\n\n")
 
     hotpy.listen()
+def test():
+
+    try:
+        os.unlink('test-out.webm')
+    except:
+        pass
+
+    name = '{}.webm'.format(namer.get_name())
+    url = 'http://i.notabigtruck.com/i/tubes/{}'.format(name)
+
+    print('Your url will be: {}'.format(url))
+
+    encode_video()
+    print('Uploading...')
+    uploader.upload('test-out.webm', name)
+
+def test2():
+
+    start_capture()
+
 
 if __name__ == '__main__':
     main()
+    #test2()
